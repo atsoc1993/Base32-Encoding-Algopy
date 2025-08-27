341cd68a9809b98d6e8bbaf87e603d681157fcf7
atsoc1993 <atsoc1993@gmail.com>
2025-08-27 17:31:22 -0400

    Adding codebase

---
+++ contract.py
@@ -0,0 +1,50 @@
+from algopy import ARC4Contract, arc4, op, Txn, urange, UInt64, String, itxn, ensure_budget, OpUpFeeSource
+from algopy.arc4 import abimethod, DynamicBytes, Byte
+
+
+class SenderToUtf8(ARC4Contract):
+    def __init__(self) -> None:
+        pass
+
+    @abimethod
+    def base32_sender_address(self) -> None:
+        ALPHABET = String("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567").bytes
+        pk = Txn.sender.bytes
+
+        h = op.sha512_256(pk)
+        cksum = h[28:32]
+
+        raw = DynamicBytes()
+        raw.extend(DynamicBytes(pk))
+        raw.extend(DynamicBytes(cksum))
+
+        data = raw.native
+        bits = UInt64(0)
+        bitlen = UInt64(0)
+        out = DynamicBytes()
+
+        for i in urange(data.length):
+            ensure_budget(required_budget=700, fee_source=OpUpFeeSource.GroupCredit)
+
+            b = arc4.UInt64.from_bytes(data[i]).native
+            bits = (bits << UInt64(8)) | b
+            bitlen += UInt64(8)
+
+            while bitlen >= UInt64(5):
+                bitlen -= UInt64(5)
+                idx = (bits >> bitlen) & UInt64(0b11111)
+                ch = ALPHABET[idx]
+                out.append(Byte(arc4.UInt64.from_bytes(ch).native))
+
+        if bitlen > UInt64(0):
+            idx = (bits << (UInt64(5) - bitlen)) & UInt64(0b11111)
+            ch = ALPHABET[idx]
+            out.append(Byte(arc4.UInt64.from_bytes(ch).native))
+
+        addr_str = String.from_bytes(out.native)
+
+        itxn.Payment(
+            amount=0,
+            receiver=Txn.sender,
+            note=addr_str
+        ).submit()
---
+++ deploy_and_test.py
@@ -0,0 +1,36 @@
+from contract_files.client import SenderToUtf8Factory
+from algosdk.account import address_from_private_key
+from algokit_utils import AlgorandClient, SigningAccount, CommonAppCallParams, AlgoAmount
+from dotenv import load_dotenv, set_key
+import os
+
+algorand = AlgorandClient.testnet()
+load_dotenv('../.env')
+sk = os.getenv('test_account_1')
+pk = address_from_private_key(sk)
+print(pk)
+signing_account = SigningAccount(
+    private_key=sk,
+    address=pk,
+)
+
+
+factory = SenderToUtf8Factory(
+    algorand=algorand,
+    default_sender=signing_account.address,
+    default_signer=signing_account.signer,
+)
+
+client, txn_response = factory.send.create.bare()
+
+txn_response = client.send.base32_sender_address(
+    params=CommonAppCallParams(
+        max_fee=AlgoAmount(algo=0.01)
+    ),
+    send_params={
+        'cover_app_call_inner_transaction_fees': True
+    }
+        
+    
+)
+print(txn_response.tx_id)
